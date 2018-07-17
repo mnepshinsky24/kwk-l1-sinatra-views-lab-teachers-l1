43f380a2311faed80216514c178d924a8356c7db
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-17 17:27:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,18 +3,7 @@ class App < Sinatra::Base
 	get '/' do
 		erb :hello 
 	end
-  
-  get '/goodbye' do 
-    erb :goodbye 
-  end
-  
-  get '/date' do 
-    erb :date
-  end 
-  
-  get '/test' do 
-    erb :testing 
-  end 
+
   
 
 end
